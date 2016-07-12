e9bee0bd6b5ec5241022314b9e44825186abc357
Vladimir <filo.vlada@gmail.com>
2016-07-12 23:34:08 +0000

    Edited a bit

--- resources/views/blog/single.blade.php
+++ resources/views/blog/single.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 
     <div class="row">
-        <div class="col-md-8 col-md-offset-2 text-center">
+        <div class="col-md-8 col-md-offset-2 title-center">
             <h1>{{ $post->title }}</h1>
             <p id='blog-content'>{{ $post->body }}</p>
         </div>
